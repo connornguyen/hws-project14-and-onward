78f85bbe52031e00c147688f7e763a31ae16d7f7
Connor Nguyen <nguyendinhthanhcong2512@gmail.com>
2024-09-11 12:38:24 -0700

    First commi

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Apple on 11/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
